288b2bbb8808c1bdb65501077fb582cfb71ea98e
puolimatkankrouvi <cde_1414@yahoo.com>
2021-09-26 19:49:04 +0300

    Updated latest React

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import CtoF from './CtoF'
 import FtoC from './FtoC';
-import {Route,Switch} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 
@@ -11,10 +15,12 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={CtoF}/>
-          <Route path="/fahrenheit-to-celcius/" component={FtoC}/>
-        </Switch>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={CtoF}/>
+            <Route path="/fahrenheit-to-celcius/" component={FtoC}/>
+          </Switch>
+        </Router>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
-render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+ReactDOM.render(
+  <App />
+, document.getElementById('root')
+);
 
 registerServiceWorker();
