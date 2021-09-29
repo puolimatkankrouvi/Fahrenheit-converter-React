a77b5970017d6024143632c784601e439baba088
puolimatkankrouvi <cde_1414@yahoo.com>
2021-09-29 07:19:00 +0300

    Using bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+    },
+    resolve: {
+        modules: [path.join(__dirname, "src"), "node_modules"],
+        alias: {
+            react: path.join(__dirname, "node_modules", "react")
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: {
+                    loader: "style-loader",
+                    loader: "css-loader",
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: "public/index.html" }),
+    ],
+    performance: {
+        hints: false,
+    }
+};
